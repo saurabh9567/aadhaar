d2067d06348ee8acfc0af8f3e32481249f582e79
saurabh9567 <saurabh956@gmail.com>
2020-05-23 22:55:13 +0530

    Update aadhaar.py

--- aadhaar.py
+++ aadhaar.py
@@ -6,7 +6,11 @@ import captcha
 
 uid_no = '541789209718'
 getcaptcha = captcha.GetCaptcha()
-driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver")
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver",chrome_options=chrome_options)
 driver.get("https://resident.uidai.gov.in/verify")
 getcaptcha.download_captcha(driver)
 captcha_text = getcaptcha.get_captcha_text()
