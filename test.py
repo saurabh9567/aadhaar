59d6e9b586d5f85768250d34e773b1b9b1277f20
saurabh9567 <saurabh956@gmail.com>
2020-05-24 14:51:44 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+import time
+import captcha
+CHROME_PATH = '/usr/bin/google-chrome-stable'
+CHROMEDRIVER_PATH = '/usr/bin/chromedriver'
+WINDOW_SIZE = "1920,1080"
+
+uid_no = '541789209718'
+getcaptcha = captcha.GetCaptcha()
+chrome_options = Options()  
+chrome_options.add_argument("--headless")  
+chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
+chrome_options.binary_location = CHROME_PATH
+
+driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH,
+                          chrome_options=chrome_options
+                         )  
+print('Driver',driver)
+# driver.get("https://resident.uidai.gov.in/verify")
+driver.get("https://www.linkedin.com/")
+driver.implicitly_wait(10)
+driver.get_screenshot_as_file("capture2.png")
+uid = driver.find_element_by_id("uidno")
+getcaptcha.download_captcha(driver)
+captcha_text = getcaptcha.get_captcha_text()
+captcha = driver.find_element_by_xpath('//*[@id="security_code"]')
+
+uid.send_keys(uid_no)
+captcha.send_keys(captcha_text)
+
+driver.implicitly_wait(5)
+driver.find_element_by_xpath('//*[@id="submitButton"]').click()
+
+elements = driver.find_element_by_xpath('//*[@id="maincontent"]/div/div[1]/div[3]')
+print(elements.text)
