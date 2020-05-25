193b1788a91dc9c8df1e0802676251421e2c41e7
saurabh9567 <saurabh956@gmail.com>
2020-05-25 14:01:27 +0530

    Update test2.py

--- test2.py
+++ test2.py
@@ -16,15 +16,18 @@ chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.binary_location = CHROME_PATH
+chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
 driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH,
                           chrome_options=chrome_options
                          )  
 print('Driver',driver)
-driver.get("https://resident.uidai.gov.in/")
+driver.get("https://resident.uidai.gov.in/verify")
 driver.implicitly_wait(15)
-print('reaced at udemy')
-driver.get_screenshot_as_file("c5.png")
+driver.get_screenshot_as_file("c7.png")
+print('reached here')
+uid = driver.find_element_by_xpath("/html/body/div[2]/div[2]/div[2]/div[1]/div/div[4]/div/div/div[2]/div/div/form/div[1]/div/div[1]/input").click()
+print('uid',uid)
 # driver.get("https://resident.uidai.gov.in/verify")
 # driver.implicitly_wait(15)
 # driver.get_screenshot_as_file("c2.png")
