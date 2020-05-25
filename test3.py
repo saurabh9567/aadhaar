d2e7ff19768454418630df54deccc828e476e275
saurabh9567 <saurabh956@gmail.com>
2020-05-25 14:41:18 +0530

    Create test3.py

---
+++ test3.py
@@ -0,0 +1,5 @@
+from selenium import webdriver
+driver = webdriver.Firefox(executable_path='usr/bin/geckodriver')
+driver.get("http://google.com")
+driver.implicitly_wait(15)
+driver.get_screenshot_as_file("c10.png")
