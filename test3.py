b2f9539914a4eadfd995cf9cf9cf2084ee4a186b
saurabh9567 <saurabh956@gmail.com>
2020-05-26 11:01:06 +0530

    Update test3.py

--- test3.py
+++ test3.py
@@ -1,5 +1,26 @@
+from pyvirtualdisplay import Display
 from selenium import webdriver
-driver = webdriver.Firefox(executable_path='/usr/bin/geckodriver')
-driver.get("http://google.com")
-driver.implicitly_wait(15)
-driver.get_screenshot_as_file("c10.png")
+
+display = Display(visible=0, size=(800, 600))
+display.start()
+
+options = webdriver.ChromeOptions()
+options.add_argument('--no-sandbox')
+
+driver = webdriver.Chrome(chrome_options=options)
+driver.get('http://nytimes.com')
+print(driver.title)
+
+
+
+
+
+
+
+
+
+# from selenium import webdriver
+# driver = webdriver.Firefox(executable_path='/usr/bin/geckodriver')
+# driver.get("http://google.com")
+# driver.implicitly_wait(15)
+# driver.get_screenshot_as_file("c10.png")
